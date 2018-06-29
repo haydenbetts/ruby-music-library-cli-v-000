5e9a3c456dc4fcc1956896f8b015aa6c8db85399
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 20:53:20 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,6 +37,8 @@ class MusicLibraryController
       list_genres
     when 'list artist'
       list_songs_by_artist
+    when 'list genre'
+      list_songs_by_genre
     end
 
     call unless input == 'exit'
